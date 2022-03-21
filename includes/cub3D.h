75e3deb2a7432f2ea839ca8318c0fadcc71bec51
Gaelle RANNOU <grannou@e2r9p14.clusters.42paris.fr>
2022-03-21 09:32:22 +0100

    add math.h + liste fonctions autorisees

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,19 +6,26 @@
 /*   By: grannou <grannou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 04:02:25 by fcatinau          #+#    #+#             */
-/*   Updated: 2022/03/21 09:22:13 by grannou          ###   ########.fr       */
+/*   Updated: 2022/03/21 09:31:51 by grannou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "get_next_line.h"
+# include "mlx.h"
 # include <unistd.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include "get_next_line.h"
-# include <stdio.h>
-# include "mlx.h"
+# include <math.h>
+
+/*
+ FONCTIONS AUTORISEES:
+open, cose, read, write, printf, malloc, free, perror, strerror, exit
+toutes les fonctions de math.h
+*/
 
 /*
 **---------------Enum---------------
