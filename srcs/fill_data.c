25926b293caae4a42928be65f1797b7d415ea410
Gaelle RANNOU <grannou@e2r9p14.clusters.42paris.fr>
2022-03-27 15:54:38 +0200

    clean file

--- srcs/fill_data.c
+++ srcs/fill_data.c
@@ -6,72 +6,12 @@
 /*   By: grannou <grannou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 13:54:09 by grannou           #+#    #+#             */
-/*   Updated: 2022/03/26 20:41:31 by grannou          ###   ########.fr       */
+/*   Updated: 2022/03/27 15:54:18 by grannou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-/*
-int		count_numbers(char *str)
-{
-	int	nbr_count;
 
-	nbr_count = 0;
-	while (str && *str)
-	{
-		if ((*str != ',') && (*(str + 1) == ',' || *(str + 1) == '\0'))
-			nbr_count++;
-		str++;
-	}
-	return (nbr_count);
-}
-
-int		ft_nbrlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (*str && *str != ',')
-		i++;
-	return (i);
-}
-
-void	split_comas(t_data **data, t_list **list, char *str, char ***array)
-{
-	int	i;
-	int	j;
-	int	nbr_count;
-	int	nbr_len;
-
-	i = 0;
-	printf("In split comas, str to split = [%s]\n", str);
-	nbr_count = count_numbers(str);
-	printf("In split comas, nbr_count = %d\n", nbr_count);
-	nbr_len = 0;
-	*array = (char **)malloc(sizeof(char *) * (nbr_count + 1));
-	if (!*array)
-		clear_all_exit(data, list, COMASFAIL);
-	while (i < nbr_count)
-	{
-		while (*str == ',')
-			str++;
-		nbr_len = ft_nbrlen(str);
-		printf("In split comas, nbr_len = %d\n", nbr_len);
-		*array[i] = (char *)malloc(sizeof(char) * (nbr_len + 1));
-		if (!*array[i])
-		{
-			free_array(*array);
-			clear_all_exit(data, list, COMASFAIL);
-		}
-		j = 0;
-		while (j < nbr_len)
-			*array[i][j++] = *str++;
-		*array[i][j] = '\0';
-		i++;
-	}
-	*array[i] = NULL;
-}
-*/
 int	get_hexa_rgb_value(t_data **data, t_list **list, char *str)
 {
 	char	**array;
