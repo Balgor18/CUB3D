601b4b98dbe080fca7b763971ca4b7e3418e1a82
Gaelle RANNOU <grannou@e1r13p9.42.fr>
2022-04-06 21:06:22 +0200

    verif done

--- srcs/mlx/create_texture.c
+++ srcs/mlx/create_texture.c
@@ -3,38 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   create_texture.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: grannou <grannou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 05:00:40 by fcatinau          #+#    #+#             */
-/*   Updated: 2022/04/06 20:06:42 by fcatinau         ###   ########.fr       */
+/*   Updated: 2022/04/06 21:05:56 by grannou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static void	xpm_file_and_addr(void *mlx_ptr, t_img *img, int byte)
+static int	xpm_file_and_addr(void *mlx_ptr, t_img *img, int byte)
 {
 	size_t	i;
 
 	i = 0;
 	img->img = mlx_new_image(mlx_ptr, WIDTH, HEIGHT / 2);
 	// do verif | PAS DE EXIT
+	if (!img->img)
+		return (EXIT_FAILURE);
 	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel,
-			&img->bits_per_pixel, &img->endian);
+			&img->line_length, &img->endian);
 	while (i < WIDTH * (HEIGHT / 2))
 	{
 		img->addr[i] = byte;
 		i++;
 	}
+	return (EXIT_SUCCESS);
 }
 
-static void	xpm_texture_and_addr(void *mlx_ptr, char *txt_name, t_img *img)
+static int	xpm_texture_and_addr(void *mlx_ptr, char *txt_name, t_img *img)
 {
 	img->img = mlx_xpm_file_to_image(mlx_ptr, txt_name, &img->width,
 			&img->height);
 	// do verif | PAS DE EXIT
+	if (!img->img)
+		return (EXIT_FAILURE);
 	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel,
 			&img->line_length, &img->endian);
+	return (EXIT_SUCCESS);
 }
 
 /**
@@ -52,16 +58,21 @@ void	create_texture(t_mlx *mlx)
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, WIDTH, HEIGHT, "CUB3D");
 	if (!mlx->win_ptr)
 		return (free_mlx(mlx));
-	xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[CEILING_IMG],
-		mlx->data->ceiling_rgb);// continue verif | PAS DE EXIT
-	xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[FLOOR_IMG],
-		mlx->data->floor_rgb);// continue verif | PAS DE EXIT
-	xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->north_texture,
-		&mlx->pict[WALL_NORTH]);// continue verif | PAS DE EXIT
-	xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->south_texture,
-		&mlx->pict[WALL_SOUTH]);// continue verif | PAS DE EXIT
-	xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->west_texture,
-		&mlx->pict[WALL_WEST]);// continue verif | PAS DE EXIT
-	xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->east_texture,
-		&mlx->pict[WALL_EAST]);// continue verif | PAS DE EXIT
+	if (xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[CEILING_IMG],
+		mlx->data->ceiling_rgb))// continue verif | PAS DE EXIT
+		return (free_mlx(mlx));
+	if (xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[FLOOR_IMG],
+		mlx->data->floor_rgb))// continue verif | PAS DE EXIT
+	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->north_texture,
+		&mlx->pict[WALL_NORTH]))// continue verif | PAS DE EXIT
+		return (free_mlx(mlx));
+	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->south_texture,
+		&mlx->pict[WALL_SOUTH]))// continue verif | PAS DE EXIT
+		return (free_mlx(mlx));
+	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->west_texture,
+		&mlx->pict[WALL_WEST]))// continue verif | PAS DE EXIT
+		return (free_mlx(mlx));
+	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->east_texture,
+		&mlx->pict[WALL_EAST]))// continue verif | PAS DE EXIT
+		return (free_mlx(mlx));
 }
