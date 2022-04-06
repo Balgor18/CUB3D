dd606e6c7eef97bc814f1bfe3399ad53f95ae782
Gaelle RANNOU <grannou@e1r13p9.42.fr>
2022-04-06 21:08:55 +0200

    norme ok

--- srcs/mlx/create_texture.c
+++ srcs/mlx/create_texture.c
@@ -6,7 +6,7 @@
 /*   By: grannou <grannou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 05:00:40 by fcatinau          #+#    #+#             */
-/*   Updated: 2022/04/06 21:05:56 by grannou          ###   ########.fr       */
+/*   Updated: 2022/04/06 21:08:40 by grannou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static int	xpm_file_and_addr(void *mlx_ptr, t_img *img, int byte)
 
 	i = 0;
 	img->img = mlx_new_image(mlx_ptr, WIDTH, HEIGHT / 2);
-	// do verif | PAS DE EXIT
 	if (!img->img)
 		return (EXIT_FAILURE);
 	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel,
@@ -35,7 +34,6 @@ static int	xpm_texture_and_addr(void *mlx_ptr, char *txt_name, t_img *img)
 {
 	img->img = mlx_xpm_file_to_image(mlx_ptr, txt_name, &img->width,
 			&img->height);
-	// do verif | PAS DE EXIT
 	if (!img->img)
 		return (EXIT_FAILURE);
 	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel,
@@ -59,20 +57,21 @@ void	create_texture(t_mlx *mlx)
 	if (!mlx->win_ptr)
 		return (free_mlx(mlx));
 	if (xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[CEILING_IMG],
-		mlx->data->ceiling_rgb))// continue verif | PAS DE EXIT
+			mlx->data->ceiling_rgb))
 		return (free_mlx(mlx));
 	if (xpm_file_and_addr(mlx->mlx_ptr, &mlx->pict[FLOOR_IMG],
-		mlx->data->floor_rgb))// continue verif | PAS DE EXIT
+			mlx->data->floor_rgb))
+		return (free_mlx(mlx));
 	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->north_texture,
-		&mlx->pict[WALL_NORTH]))// continue verif | PAS DE EXIT
+			&mlx->pict[WALL_NORTH]))
 		return (free_mlx(mlx));
 	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->south_texture,
-		&mlx->pict[WALL_SOUTH]))// continue verif | PAS DE EXIT
+			&mlx->pict[WALL_SOUTH]))
 		return (free_mlx(mlx));
 	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->west_texture,
-		&mlx->pict[WALL_WEST]))// continue verif | PAS DE EXIT
+			&mlx->pict[WALL_WEST]))
 		return (free_mlx(mlx));
 	if (xpm_texture_and_addr(mlx->mlx_ptr, mlx->data->east_texture,
-		&mlx->pict[WALL_EAST]))// continue verif | PAS DE EXIT
+			&mlx->pict[WALL_EAST]))
 		return (free_mlx(mlx));
 }
